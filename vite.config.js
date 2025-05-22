1ffa90fa24d2639476c7b78a0bb8787347cf20ae
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-05-22 17:08:49 +0200

    Premier commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
+import pages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/atlanta-ci/',
+  plugins: [react(), vue(), pages()],
 })
